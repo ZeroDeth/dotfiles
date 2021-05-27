116f25626fd283c80ca61d25f5b297f70a1e2d9b
Sherif Abdalla <sherif@abdalla.uk>
2021-05-27 19:32:02 +0100

    Update space prompt

--- .zsh_plugins.sh
+++ .zsh_plugins.sh
@@ -1,52 +1,54 @@
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-jvm/jvm.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-jvm )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-mkc/mkc.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-mkc )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-open-github-pr/git-open-pr.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-open-github-pr )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-greymd-SLASH-tmux-xpanes/tmux-xpanes.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-greymd-SLASH-tmux-xpanes )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd/init.sh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd )
-
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-junegunn-SLASH-fzf/shell/completion.zsh
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-junegunn-SLASH-fzf/shell/key-bindings.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-junegunn-SLASH-fzf/shell )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bbenne10-SLASH-goenv/goenv.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bbenne10-SLASH-goenv )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git/git.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pip/pip.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pip )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/last-working-dir/last-working-dir.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/last-working-dir )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found/command-not-found.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/common-aliases/common-aliases.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/common-aliases )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/compleat/compleat.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/compleat )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git-extras/git-extras.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git-extras )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/osx/osx.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/osx )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pyenv/pyenv.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pyenv )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/python/python.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/python )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/tmuxinator/tmuxinator.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/tmuxinator )
-
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/web-search/web-search.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/web-search )
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-docker-SLASH-cli/contrib/completion/zsh )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
-source /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-denysdovhan-SLASH-spaceship-prompt/spaceship.zsh
-fpath+=( /Users/ZeroDeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-denysdovhan-SLASH-spaceship-prompt )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-jvm/jvm.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-jvm )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-mkc/mkc.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-mkc )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-open-github-pr/git-open-pr.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-open-github-pr )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-greymd-SLASH-tmux-xpanes/tmux-xpanes.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-greymd-SLASH-tmux-xpanes )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd/enhancd.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zchee-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zchee-SLASH-zsh-completions )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-junegunn-SLASH-fzf/shell/completion.zsh
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-junegunn-SLASH-fzf/shell/key-bindings.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-junegunn-SLASH-fzf/shell )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bbenne10-SLASH-goenv/goenv.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bbenne10-SLASH-goenv )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git/git.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pip/pip.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pip )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/last-working-dir/last-working-dir.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/last-working-dir )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found/command-not-found.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/common-aliases/common-aliases.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/common-aliases )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/compleat/compleat.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/compleat )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git-extras/git-extras.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git-extras )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/osx/osx.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/osx )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pyenv/pyenv.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pyenv )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/python/python.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/python )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/tmuxinator/tmuxinator.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/tmuxinator )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/vagrant/vagrant.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/vagrant )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/web-search/web-search.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/web-search )
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-docker-SLASH-cli/contrib/completion/zsh )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-denysdovhan-SLASH-spaceship-prompt/spaceship.zsh
+fpath+=( /Users/zerodeth/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-denysdovhan-SLASH-spaceship-prompt )
