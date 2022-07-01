d81d017dceef464c2d25791a9bb72f942b5490d0
Sherif Abdalla <ZeroDeth@users.noreply.github.com>
2022-07-01 20:12:50 +0100

    Create Script to Install Chezomi & Brew
    This'll install Homebrew and chezmoi. After installation chezmoi will be initialized.

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+echo ""
+echo "🤚  This script will setup .dotfiles for you."
+read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n\n'
+
+
+# Install Homebrew
+command -v brew >/dev/null 2>&1 || \
+  (echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+
+# Install Oh My Zsh
+# if [ ! -f ~/.oh-my-zsh/oh-my-zsh.sh ]; then
+#   (echo '💰  Installing oh-my-zsh' && yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
+# fi
+
+# Install chezmoi
+command -v chezmoi >/dev/null 2>&1 || \
+  (echo '👊  Installing chezmoi' && brew install chezmoi)
+
+if [ -d "$HOME/.local/share/chezmoi/.git" ]; then
+  echo "🚸  chezmoi already initialized"
+  echo "    Reinitialize with: 'chezmoi init https://github.com/zerodeth/dotfiles.git'"
+else
+  echo "🚀  Initialize dotfiles with:"
+  echo "    chezmoi init https://github.com/zerodeth/dotfiles.git"
+fi
+
+echo ""
+echo "Done."
