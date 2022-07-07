f812d8f77e21dc5ac48fd6e713c7f344c151b935
Sherif Abdalla <10928577+ZeroDeth@users.noreply.github.com>
2022-07-07 05:03:53 +0000

    remove bitwarden

--- .chezmoiscripts/run_once_pass.sh
+++ .chezmoiscripts/run_once_pass.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 # SAVE SECRETS LOCALLY
-bw get item 'StandardNotes' | jq -r '.login.username' | pass insert --echo --force 'sn/mail'
-bw get item 'StandardNotes' | jq -r '.login.password' | pass insert --echo --force 'sn/pass'
