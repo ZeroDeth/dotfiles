3452344f2864a3e1a6e089f3c2034442dac61f2e
Sherif Abdalla <sherif@abdalla.uk>
2024-01-19 12:27:01 +0000

    Update Nix settings and package configuration

--- home/default.nix
+++ home/default.nix
@@ -1,5 +1,5 @@
 # Better managaement https://github.com/srid/nixos-config/blob/master/home/default.nix
-{ flake, ... }:
+{ flake, pkgs, misc, lib, config, ... }:
 {
   imports = [
     # This loads ./neovim/default.nix - neovim configured for Haskell dev, and other things.
@@ -16,6 +16,41 @@
 
   # Recommended Nix settings
   nix = {
+    package = pkgs.nixFlakes;
+    settings = {
+      trusted-users = [ "root" "zeroddeth" ];
+
+      substituters = [
+        # cache mirror located in China
+        # status: https://mirror.sjtu.edu.cn/
+        # "https://mirror.sjtu.edu.cn/nix-channels/store"
+        # status: https://mirrors.ustc.edu.cn/status/
+        # "https://mirrors.ustc.edu.cn/nix-channels/store"
+
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://nix-gaming.cachix.org"
+        "https://chaotic-nyx.cachix.org"
+        "https://ezkea.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        # the default public key of cache.nixos.org, it's built-in, no need to add it here
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
+      ];
+
+      accept-flake-config = true;
+      auto-optimise-store = true;
+    };
+
+    extraOptions = lib.optionalString (config.nix.package == pkgs.nixFlakes)
+       "experimental-features = nix-command flakes";
+
     registry.nixpkgs.flake = flake.inputs.nixpkgs; # https://yusef.napora.org/blog/pinning-nixpkgs-flake/
   };
 }
