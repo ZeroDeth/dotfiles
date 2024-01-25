964bf0eaa6892e74ab9ad33bab90b73a7d3914f7
Sherif Abdalla <sherif@abdalla.uk>
2024-01-25 15:07:56 +0000

    Remove unnecessary bash configuration in terminal.nix

--- home/programs/shells/terminal.nix
+++ home/programs/shells/terminal.nix
@@ -35,15 +35,6 @@
       ];
     };
 
-    # on macOS, you probably don't need this
-    bash = {
-      enable = true;
-      initExtra = ''
-        # Make Nix and home-manager installed things available in PATH.
-        export PATH=/run/current-system/sw/bin/:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
-      '';
-    };
-
     # https://zero-to-flakes.com/direnv
     direnv = {
       enable = true;
