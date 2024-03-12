744f7c8f7249c563621f3848d0f540559598ee8d
Sherif Abdalla <sherif@abdalla.uk>
2024-03-12 15:03:06 +0000

    Disable 'allowBroken'

--- home/default.nix
+++ home/default.nix
@@ -85,7 +85,7 @@ in
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = (_: true);
 
-      allowBroken = true;
+      allowBroken = false;
     };
   };
 
