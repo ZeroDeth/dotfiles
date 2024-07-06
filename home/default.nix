9ea73c1f3082b7a3efea81bda086e56c10a4d0cd
Sherif Abdalla <sherif@abdalla.uk>
2024-07-06 19:40:58 +0100

    Refactor to use other-packages

--- home/default.nix
+++ home/default.nix
@@ -20,108 +20,7 @@ let
     nix-symlink-apps-macos
   ];
 
-  stable-packages = with pkgs-stable; [
-    # Your stable packages here
-    nh
-
-  ];
-
-in
-{
-  imports = [
-    # ./dotfiles
-    ./programs
-    # Add more of your home-manager modules here.
-    ./modules
-
-  ];
-
-  # Recommended Nix settings https://github.com/NixOS/nixpkgs/blob/nixos-23.11/nixos/modules/config/nix.nix
-  nix = {
-    package = pkgs.nixFlakes;
-    settings = {
-      trusted-users = [ "root" "zerodeth" ];
-
-      substituters = [
-        # cache mirror located in China
-        # status: https://mirror.sjtu.edu.cn/
-        # "https://mirror.sjtu.edu.cn/nix-channels/store"
-        # status: https://mirrors.ustc.edu.cn/status/
-        # "https://mirrors.ustc.edu.cn/nix-channels/store"
-
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://nix-gaming.cachix.org"
-        "https://chaotic-nyx.cachix.org"
-        "https://ezkea.cachix.org"
-        "https://ai.cachix.org"
-      ];
-
-      trusted-public-keys = [
-        # the default public key of cache.nixos.org, it's built-in, no need to add it here
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-        "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
-        "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
-      ];
-
-      accept-flake-config = true;
-      auto-optimise-store = true;
-
-      keep-outputs = false; # Nice for developers, if true
-      keep-derivations = false; # Idem, if true
-    };
-
-    # nix show-derivation --extra-experimental-features nix-command $(ls /nix/store/*.drv | head -n 1)
-    # nix show-derivation --extra-experimental-features nix-command $(find /nix/store -maxdepth 1 -name '*.drv' | head -n 1)
-    extraOptions = lib.optionalString (config.nix.package == pkgs.nixFlakes)
-      "experimental-features = nix-command flakes";
-
-    registry.nixpkgs.flake = flake.inputs.nixpkgs; # https://yusef.napora.org/blog/pinning-nixpkgs-flake/
-  };
-
-  nixpkgs = {
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = (_: true);
-
-      allowBroken = false;
-    };
-  };
-
-  home.sessionVariables = {
-    LANG = "en_GB.UTF-8";
-    LC_CTYPE = "en_GB.UTF-8";
-    LC_ALL = "en_GB.UTF-8";
-    EDITOR = "nvim";
-    PAGER = "less -FirSwX";
-    # MANPAGER = "${manpager}/bin/manpager";
-    # NIXPKGS_ALLOW_UNFREE= "1";
-    # NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM= "1";
-    # SSH_AUTH_SOCK = "~/.1password/agent.sock";
-    # OP_BIOMETRIC_UNLOCK_ENABLED = "true";
-  };
-
-  home.sessionPath = [
-    "$HOME/bin"
-    "$HOME/.local/bin"
-    "$HOME/.gnupg"
-    "/opt/homebrew/bin"
-    "/opt/homebrew/sbin"
-    "$HOME/.local/share/mise/shims"
-  ];
-
-  ## Nix packages to install to $HOME
-  #
-  ## Search for packages here: https://search.nixos.org/packages
-  home.packages = with pkgs; [
-    # Your unstable packages
+  unstable-packages = with pkgs; [
 
     ## Unix tools
     ripgrep # Better `grep`
@@ -216,10 +115,118 @@ in
 
     ## Kubernetes
     # k9s
+
+    (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
+  ];
+
+  stable-packages = with pkgs-stable; [
+
+    nh
+
+  ];
+
+  other-packages = with pkgs; [
+
     ksctl # TODO: Overlays not working
 
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
-  ] ++ stable-packages;
+  ];
+
+in
+{
+  imports = [
+    # ./dotfiles
+    ./programs
+    # Add more of your home-manager modules here.
+    ./modules
+
+  ];
+
+  # Recommended Nix settings https://github.com/NixOS/nixpkgs/blob/nixos-23.11/nixos/modules/config/nix.nix
+  nix = {
+    package = pkgs.nixFlakes;
+    settings = {
+      trusted-users = [ "root" "zerodeth" ];
+
+      substituters = [
+        # cache mirror located in China
+        # status: https://mirror.sjtu.edu.cn/
+        # "https://mirror.sjtu.edu.cn/nix-channels/store"
+        # status: https://mirrors.ustc.edu.cn/status/
+        # "https://mirrors.ustc.edu.cn/nix-channels/store"
+
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://nix-gaming.cachix.org"
+        "https://chaotic-nyx.cachix.org"
+        "https://ezkea.cachix.org"
+        "https://ai.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        # the default public key of cache.nixos.org, it's built-in, no need to add it here
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
+        "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
+      ];
+
+      accept-flake-config = true;
+      auto-optimise-store = true;
+
+      keep-outputs = false; # Nice for developers, if true
+      keep-derivations = false; # Idem, if true
+    };
+
+    # nix show-derivation --extra-experimental-features nix-command $(ls /nix/store/*.drv | head -n 1)
+    # nix show-derivation --extra-experimental-features nix-command $(find /nix/store -maxdepth 1 -name '*.drv' | head -n 1)
+    extraOptions = lib.optionalString (config.nix.package == pkgs.nixFlakes)
+      "experimental-features = nix-command flakes";
+
+    registry.nixpkgs.flake = flake.inputs.nixpkgs; # https://yusef.napora.org/blog/pinning-nixpkgs-flake/
+  };
+
+  nixpkgs = {
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = (_: true);
+
+      allowBroken = false;
+    };
+  };
+
+  home.sessionVariables = {
+    LANG = "en_GB.UTF-8";
+    LC_CTYPE = "en_GB.UTF-8";
+    LC_ALL = "en_GB.UTF-8";
+    EDITOR = "nvim";
+    PAGER = "less -FirSwX";
+    # MANPAGER = "${manpager}/bin/manpager";
+    # NIXPKGS_ALLOW_UNFREE= "1";
+    # NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM= "1";
+    # SSH_AUTH_SOCK = "~/.1password/agent.sock";
+    # OP_BIOMETRIC_UNLOCK_ENABLED = "true";
+  };
+
+  home.sessionPath = [
+    "$HOME/bin"
+    "$HOME/.local/bin"
+    "$HOME/.gnupg"
+    "/opt/homebrew/bin"
+    "/opt/homebrew/sbin"
+    "$HOME/.local/share/mise/shims"
+  ];
+
+  home.packages =
+    unstable-packages
+    ++ stable-packages
+    ++ other-packages;
 
   fonts.fontconfig.enable = true;
 
